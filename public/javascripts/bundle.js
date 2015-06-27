d099c37e5bdab2e1571bd5f3b0dd57701e510b8f
Wayne Simmerson <wsimmerson@gmail.com>
2015-06-27 13:47:03 -0400

    ping update taks

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -93,10 +93,7 @@ angular.module('ItsAliveApp')
       scope: {},
       templateUrl: "partials/front.html",
       controller: function ($scope, $http, $interval) {
-        $scope.services = [
-          {"name": "RevolutionIP", "address":"8605 Twin Oaks Drive, windsor, ontario", "Lat": 42.2928590, "Long": -82.9147780, "status": "up"},
-          {"name": "Mayson Machine", "address":"3706 sandwich st. w, windsor, ontario", "Lat": 42.2917970, "Long": -83.0824950, "status": "down"}
-        ];
+        $scope.services = [];
 
         $scope.init = function() {
           $http.get("/api/")
